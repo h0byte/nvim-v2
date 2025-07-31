d822df69d03770a1fd55c5a9c3733e1a15522ed2
h0byte <honzahladkej@gmail.com>
2025-07-31 22:21:22 +0200

    feat: added comfy line numbers

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = LazyVim.safe_keymap_set
+
+map("n", "<leader>e", function()
+  Snacks.explorer()
+end, { desc = "Terminal (cwd)" })
+
+map("n", "<leader>o", function()
+  Snacks.terminal()
+end, { desc = "Terminal (cwd)" })
